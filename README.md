2d2e4b21197814e4caa877346f48d87b48c08911
Mxz <954380531@qq.com>
2019-12-27 16:58:04 +0800

    fix 增加管理平台相关接口

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Server
 博客项目-node服务端
+
+> 启动方法
+`yarn dev`
---
+++ app/controller/admin/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class MainController extends Controller {
+  async index() {
+    this.ctx.body = {
+      data: null,
+      success: true,
+      description: 'hello admin',
+    };
+  }
+
+  // 验证登录
+  async checkLogin() {
+    const userName = this.ctx.request.body.userName;
+    const password = this.ctx.request.body.password;
+    const sql = `SELECT userName 
+                FROM user WHERE 
+                userName = ${userName} AND 
+                password = ${password}`;
+    const result = await this.app.mysql.query(sql);
+    if (result.length > 0) {
+      // 登录成功，进行session缓存
+      const openId = new Date().getTime();
+      this.ctx.session.openId = { openId };
+      this.ctx.body = {
+        data: openId,
+        success: true,
+        description: '登录成功',
+      };
+    } else {
+      this.ctx.body = {
+        data: null,
+        success: false,
+        description: '登录失败',
+      };
+    }
+  }
+}
+
+module.exports = MainController;
